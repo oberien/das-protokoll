e7dde3138b49a10f76f8839df6b124038da0e26f
main() <main@ehvag.de>
2018-04-29 15:33:27 +0200

    imporovementsa

--- csync/src/client.rs
+++ csync/src/client.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{Write, Cursor};
 use std::time::{Instant, Duration};
 use std::net::SocketAddr;
 use std::fs::File as StdFile;
@@ -12,9 +12,10 @@ use tokio::runtime::Runtime;
 use tokio::reactor::PollEvented2 as PollEvented;
 use tokio_file_unix::File;
 use tokio::io;
+use byteorder::{ReadBytesExt, WriteBytesExt, LE};
 
-const MTU: usize = 1460;
-const CHUNK_SIZE: usize = 16;
+use codec::*;
+
 pub fn client() -> Result<(), Error>  {
     let socket = UdpSocket::bind(&"127.0.0.1:0".parse().unwrap())?;
     let server = &"127.0.0.1:21088".parse().unwrap();
@@ -28,27 +29,27 @@ pub fn client() -> Result<(), Error>  {
     let filename = "/etc/passwd";
     let file = StdFile::open(filename).unwrap();
     let filesize = file.metadata().unwrap().len() as usize; // FIXME usize wtf
-    let chunk_count = (filesize + CHUNK_SIZE - 1) / CHUNK_SIZE;
-    let last_chunk_size = filesize - (chunk_count - 1) * CHUNK_SIZE;
+
+    let chunk_size = index_field_size(MTU, filesize);
+    let chunk_count = (filesize + chunk_size - 1) / chunk_size;
+    let last_chunk_size = filesize - (chunk_count - 1) * chunk_size;
     let file = File::new_nb(file).unwrap().into_io(runtime.reactor()).unwrap();
 
-    writeln!(send_buf, "{}", filename).unwrap();
+    Login { client_token: b"roflcopter" }.encode(&mut send_buf);
 
     let client = socket.send_dgram(send_buf, server)
         .and_then(move |(socket, send_buf)| {
             let syn_send_stamp = Instant::now();
             socket.recv_dgram(recv_buf).map(move |x| (x, send_buf, syn_send_stamp))
         })
-        .and_then(|((socket, recv_buf, recv_len, server), mut send_buf, syn_send_stamp)| {
+        .and_then(move |((socket, recv_buf, recv_len, server), mut send_buf, syn_send_stamp)| {
             // TODO: process contents of this packet?
             let ack_recv_stamp = Instant::now();
             let rtt = ack_recv_stamp - syn_send_stamp;
             // assert_eq!(recv_len, 0); // ???
 
             send_buf.clear();
-            send_buf.push(0);
-            send_buf.push(4);
-            send_buf.extend_from_slice(b"yolo");
+            Command::UploadRequest(UploadRequest { path: filename, length: filesize }).encode(&mut send_buf);
 
             socket.send_dgram(send_buf, &server).map(move |x| (x, recv_buf, server, rtt))
         })
@@ -65,19 +66,27 @@ pub fn client() -> Result<(), Error>  {
                 chunk_bitmap: BitVec::from_elem(chunk_count, false),
                 chunk_cursor: 0,
                 last_chunk_size,
-            }, |Client { socket, server, mut send_buf, recv_buf, rtt, file, chunk_bitmap, chunk_cursor, last_chunk_size }| {
+            }, move |Client { socket, server, mut send_buf, recv_buf, rtt, file, chunk_bitmap, chunk_cursor, last_chunk_size }| {
                 if chunk_cursor == chunk_bitmap.len() {
                     Box::new(ok(Loop::Break(()))) as Box<Future<Item=_, Error=_> + Send>
-                    //unimplemented!();
                 } else {
                     send_buf.clear();
-                    send_buf.extend_from_slice(&[0; CHUNK_SIZE]);
-                    if chunk_cursor == chunk_bitmap.len() - 1 {
-                        send_buf.truncate(last_chunk_size);
-                    }
+
+                    let payload = if chunk_cursor == chunk_bitmap.len() - 1 {
+                        last_chunk_size
+                    } else {
+                        chunk_size
+                    };
+                    send_buf.resize(payload, 0);
+
                     Box::new(
                         io::read_exact(file, send_buf)
                             .and_then(move |(file, send_buf)| {
+                                let mut arr = [0u8; 8];
+                                (&mut arr[..]).write_u64::<LE>(chunk_cursor as u64).unwrap();
+
+                                let send_buf: Vec<u8> = arr.iter().cloned().take(index_field_size(MTU, filesize)).chain(send_buf.into_iter()).collect();
+
                                 let server = server;
                                 socket.send_dgram(send_buf, &server).map(move |x| (x, file, server))
                             })
