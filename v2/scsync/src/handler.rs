d0e6d50172e8e3818317ccea94aebf3392b13c17
oberien <jaro.fietz@gmx.de>
2018-07-13 15:48:50 +0200

    wip

---
+++ v2/scsync/src/handler.rs
@@ -0,0 +1,125 @@
+use std::rc::Rc;
+use std::cell::{RefCell, Ref};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::io;
+use std::time::{Instant, Duration};
+
+use futures::{Future, IntoFuture};
+use futures::future::{self, Either};
+
+use blockdb::{BlockDb, BlockId};
+use codec::{RootUpdate, RootUpdateResponse, Msg, BlockRequest, BlockRequestResponse,
+    TransferPayload, TransferStatus};
+use futures::unsync::{mpsc, oneshot};
+use tokio::timer::Delay;
+
+#[derive(Default)]
+struct Client {
+    pending_block_requests: HashMap<BlockId, oneshot::Sender<Msg>>,
+}
+
+pub enum ClientState {
+    Known,
+    New,
+}
+
+pub struct Handler<'a> {
+    blockdb: Rc<RefCell<BlockDb>>,
+    clients: Rc<RefCell<HashMap<SocketAddr, Client>>>,
+    tx: &'a mpsc::Sender<(Msg, SocketAddr)>,
+}
+
+impl<'a> Handler<'a> {
+    pub fn new(blockdb: BlockDb, tx: &'a mpsc::Sender<(Msg, SocketAddr)>) -> Handler {
+        Handler {
+            blockdb: Rc::new(RefCell::new(blockdb)),
+            clients: Rc::new(RefCell::new(HashMap::new())),
+            tx,
+        }
+    }
+
+    pub fn client_state(&self, addr: &SocketAddr) -> ClientState {
+        match self.clients.borrow().contains_key(addr) {
+            true => ClientState::Known,
+            false => ClientState::New,
+        }
+    }
+
+    fn client(&self, addr: &SocketAddr) -> &mut Client {
+        self.clients.borrow_mut().get_mut(&addr).unwrap()
+    }
+
+    pub fn unconnected_root_update(&self, addr: SocketAddr, update: RootUpdate) -> impl Future {
+        let response = if update.to_blockref.blockid == self.blockdb.borrow().root().blockid {
+            // nothing to do, just respond
+            let blockid = self.blockdb.borrow().root().blockid;
+            let key = self.blockdb.borrow().root().key;
+            Msg::RootUpdateResponse(RootUpdateResponse {
+                from_blockid: blockid,
+                to_blockid: blockid,
+                to_key: key,
+            })
+        } else {
+            // open connection
+            let client = self.clients.borrow_mut().insert(addr, Client::default());
+            // TODO: traverse tree, request packets
+            unimplemented!()
+        };
+        self.tx.clone().send((response, addr))
+            .map(|_sender| ()).map_err(|_| unreachable!())
+    }
+
+    pub fn needs_update(&self, res: RootUpdateResponse) -> bool {
+        self.blockdb.borrow().root().blockid != res.to_blockid
+    }
+
+    pub fn root_update_response(&self, addr: SocketAddr, res: RootUpdateResponse) -> impl Future {
+        // prepare to receive a block request response, then send one out
+        let (otx, orx) = oneshot::channel();
+        self.client(&addr).pending_block_requests.insert([0; 32], otx);
+        // todo: be "smart" about concurrency
+        // consider: A-B, A-C, B-D, C-D
+        // an update from A goes to B and C perhaps simultaneously,
+        // leading to B and C concurrently advertising the same state to D
+        // ideally D would receive half of the blocks from B and the other half from C
+        // however this requires cross-connection reasoning
+
+        let tx = self.tx.clone();
+        request_retry(orx, move || tx.send((Msg::BlockRequest(unimplemented!()), addr.clone())).map(|_| ()))
+            .map(|msg| ())
+    }
+
+    pub fn block_request(&self, addr: SocketAddr, req: BlockRequest) -> impl Future {
+        self.tx.clone().send((Msg::BlockRequestResponse(unimplemented!()), addr))
+            .map(|_sender| ()).map_err(|_| unreachable!())
+    }
+
+    pub fn block_request_response(&self, addr: SocketAddr, res: BlockRequestResponse) {
+        if let Some(task) = self.client(&addr).pending_block_requests.remove(&[0; 32]) {
+            task.send(Msg::BlockRequestResponse(res)).unwrap();
+        }
+    }
+
+    pub fn transfer_payload(&self, addr: SocketAddr, payload: TransferPayload) {
+        unimplemented!()
+    }
+
+    pub fn transfer_status(&self, addr: SocketAddr, status: TransferStatus) {
+        unimplemented!()
+    }
+}
+
+fn request_retry<T, F, B>(rx: oneshot::Receiver<T>, mut f: F) -> impl Future<Item = T, Error = io::Error>
+    where F: FnMut() -> B, B: IntoFuture<Item = ()> {
+    future::loop_fn(rx, move |rx| {
+        f().into_future()
+            .map_err(|_| unimplemented!())
+            .and_then(move |()| rx.select2(Delay::new(Instant::now() + Duration::from_secs(1))))
+            .map(|x| match x {
+                Either::A((response, _delay)) => future::Loop::Break(response),
+                Either::B(((), orx)) => future::Loop::Continue(orx),
+            })
+            .map_err(|_| None.unwrap())
+    })
+}
--- v2/scsync/src/main.rs
+++ v2/scsync/src/main.rs
@@ -13,6 +13,7 @@ extern crate tiny_keccak;
 extern crate crypto;
 extern crate rand;
 extern crate aesstream;
+
 use tokio::prelude::*;
 use tokio::net::{UdpSocket, UdpFramed};
 use tokio::timer::Delay;
@@ -24,34 +25,18 @@ use std::usize;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
 use std::time::{Instant, Duration};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 mod frontend;
 mod blockdb;
 mod codec;
+mod handler;
 
 use codec::{Msg, MyCodec, RootUpdateResponse};
-
-use blockdb::BlockId;
+use blockdb::{BlockId, BlockDb};
 use frontend::Frontend;
-
-#[derive(Default)]
-struct Client {
-    pending_block_requests: HashMap<BlockId, oneshot::Sender<Msg>>,
-}
-
-fn request_retry<T, F, B>(rx: oneshot::Receiver<T>, mut f: F) -> impl Future<Item = T, Error = std::io::Error>
-    where F: FnMut() -> B, B: IntoFuture<Item = ()> {
-    future::loop_fn(rx, move |rx| {
-        f().into_future()
-            .map_err(|_| unimplemented!())
-            .and_then(move |()| rx.select2(Delay::new(Instant::now() + Duration::from_secs(1))))
-            .map(|x| match x {
-                A((response, _delay)) => future::Loop::Break(response),
-                B(((), orx)) => future::Loop::Continue(orx),
-            })
-            .map_err(|_| None.unwrap())
-    })
-}
+use handler::{Handler, ClientState};
 
 fn main() {
     let frontend = Frontend::from_folder("foo");
@@ -60,8 +45,6 @@ fn main() {
     panic!("nyi");
     let blockdb = frontend.into_inner();
 
-    let mut clients = HashMap::new();
-
     let addr = "127.0.0.1:12345".parse().unwrap();
     let socket = UdpSocket::bind(&addr).unwrap();
 
@@ -69,7 +52,7 @@ fn main() {
     let (utx, rx) = framed.split();
     let (tx, crx) = mpsc::channel(1); // would like this to be 0 but impossibruh
 
-    let tx = &tx;
+    let mut handler = Handler::new(blockdb, &tx);
 
     // omfg give bang type already !!!!!!!!
     let crx = crx.map_err(|()| None.unwrap());
@@ -77,77 +60,46 @@ fn main() {
     // cant happen
     //let tx = tx.sink_map_err(|_| { unreachable!(); std::io::Error::new(std::io::ErrorKind::AddrInUse, "") });
 
+    // TODO: wait according to send rate
     let send_task = crx.forward(utx).map(|(_, _)| ());
 
     let recv_task = rx.map(|(msg, addr)| {
-        match clients.entry(addr.clone()) {
-            Entry::Vacant(v) => A(match msg {
+        match handler.client_state(&addr) {
+            ClientState::New => A(match msg {
                 Msg::RootUpdate(update) => {
-                    let response = if update.to_blockref.blockid == blockdb.root().blockid {
-                        // nothing to do, just respond
-                        Msg::RootUpdateResponse(RootUpdateResponse {
-                            from_blockid: blockdb.root().blockid,
-                            to_blockid: blockdb.root().blockid,
-                            to_key: blockdb.root().key,
-                        })
-                    } else {
-                        // open connection
-                        let client = v.insert(Client::default());
-                        // TODO: traverse tree, request packets
-                        unimplemented!()
-                    };
-                    A(tx.clone().send((response, addr))
-                        .map(|_sender| ()).map_err(|_| unreachable!()))
+                    A(handler.unconnected_root_update(addr, update))
                 },
                 _ => B(future::ok(())), // ignore all other messages from a node we don't know
             }),
-            Entry::Occupied(mut o) => B({
-                let client = o.get_mut();
-
+            ClientState::Known => B({
                 match msg {
-                    // not sure how to handle root updates here??
+                    // TODO: not sure how to handle root updates here??
                     Msg::RootUpdate(update) => A(future::ok(())),
-
                     Msg::RootUpdateResponse(res) => {
-                        if true {
+                        if !handler.needs_update(res) {
                             // nothing to do
                             A(future::ok(()))
                         } else {
                             // we need to update
-
-                            // prepare to receive a block request response, then send one out
-                            let (otx, orx) = oneshot::channel();
-                            client.pending_block_requests.insert([0; 32], otx);
-                            // todo: be "smart" about concurrency
-                            // consider: A-B, A-C, B-D, C-D
-                            // an update from A goes to B and C perhaps simultaneously,
-                            // leading to B and C concurrently advertising the same state to D
-                            // ideally D would receive half of the blocks from B and the other half from C
-                            // however this requires cross-connection reasoning
-
-                            B(A(
-                                request_retry(orx, move || tx.clone().send((Msg::BlockRequest(unimplemented!()), addr.clone())).map(|_| ()))
-                                    .map(|msg| ())
-                            ))
+                            B(A(handler.root_update_response(addr, res)))
                         }
                     }
                     Msg::BlockRequest(req) => {
                         // do things, then send response
-                        B(B(tx.clone().send((Msg::BlockRequestResponse(unimplemented!()), addr))
-                            .map(|_sender| ()).map_err(|_| unreachable!())))
+                        B(B(handler.block_request(addr, req)))
                     }
-                    Msg::BlockRequestResponse(_) => {
-                        if let Some(task) = client.pending_block_requests.remove(&[0; 32]) {
-                            task.send(msg).unwrap();
-                        }
+                    Msg::BlockRequestResponse(res) => {
+                        handler.block_request_response(addr, res);
                         A(future::ok(()))
                     }
                     Msg::TransferPayload(payload) => {
                         // receive data
+                        handler.transfer_payload(addr, payload);
                         A(future::ok(()))
                     }
                     Msg::TransferStatus(status) => {
                         // update transfer status
+                        handler.transfer_status(addr, status);
                         A(future::ok(()))
                     }
                 }
@@ -166,4 +118,3 @@ fn main() {
 
     println!("{:?}", res);
 }
-
